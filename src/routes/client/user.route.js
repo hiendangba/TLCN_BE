498af0e44b95aaa401aa2439325d1679a3663db3
hiendangba <22110320@student.hcmute.edu.vn>
2025-11-14 16:21:52 +0700

    implement refresh token logic, logout, and reset password flow

--- src/routes/client/user.route.js
+++ src/routes/client/user.route.js
@@ -3,9 +3,9 @@ const userRouter = express.Router();
 const { authMiddleware } = require("../../middlewares/auth.middleware");
 const validateRequest = require("../../middlewares/validateRequest");
 const userController = require("../../controllers/user.controller")
-const { userChangePasswordSchema } = require("../../validations/user.validation")
+const { userChangePasswordSchema, userUpdateProfileSchema } = require("../../validations/user.validation")
 userRouter.get("/", authMiddleware, userController.getUser);
 userRouter.patch("/change-password", authMiddleware, validateRequest(userChangePasswordSchema), userController.changePassword)
-userRouter.patch("/", authMiddleware, validateRequest(userChangePasswordSchema), userController.changePassword)
+userRouter.patch("/profile", authMiddleware, validateRequest(userUpdateProfileSchema), userController.updateProfile)
 
 module.exports = userRouter;
