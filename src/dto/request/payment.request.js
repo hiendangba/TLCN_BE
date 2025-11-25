94aecd422a2d4bd0b24b501e9e3cc0d87038cb31
phamhoanghuy2004 <phamhoanghuy.2000vn@gmail.com>
2025-11-25 20:25:19 +0700

    Huy update

--- src/dto/request/payment.request.js
+++ src/dto/request/payment.request.js
@@ -4,4 +4,14 @@ class PaymentRequest {
     }
 }
 
-module.exports = { PaymentRequest };
+class GetPaymentByUserId {
+    constructor(data) {
+        this.userId = data.userId;
+        this.type = data.type;
+        this.page = data.page ? Number(data.page) : 1;
+        this.limit = data.limit ;
+        this.keyword = data.keyword ? data.keyword.trim() : "";
+    }
+}
+
+module.exports = { PaymentRequest, GetPaymentByUserId };
